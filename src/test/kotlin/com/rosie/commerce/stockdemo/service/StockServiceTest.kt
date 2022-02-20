3f08858c585e8061e1c7f0c7069c86f5510ed6b1
kayoung yoon <rkdkdudjd@gmail.com>
2022-02-21 00:07:51 +0900

    test: 상품 재고 계산 redis 테스트

--- src/test/kotlin/com/rosie/commerce/stockdemo/service/StockServiceTest.kt
+++ src/test/kotlin/com/rosie/commerce/stockdemo/service/StockServiceTest.kt
@@ -10,132 +10,184 @@ import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.SpyK
 import io.mockk.junit5.MockKExtension
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.ReactiveRedisTemplate
 
 @ExtendWith(MockKExtension::class)
 class StockServiceTest {
-    @SpyK
-    @InjectMockKs
-    private lateinit var stockService: StockService
+    internal class MockStockRepository {
+        @SpyK
+        @InjectMockKs
+        private lateinit var stockService: StockService
 
-    @MockK
-    @InjectMockKs
-    private lateinit var stockStateEventPublisher: StockStateEventPublisher
+        @MockK
+        @InjectMockKs
+        private lateinit var stockStateEventPublisher: StockStateEventPublisher
 
-    @MockK
-    private lateinit var stockRepository: StockRepository
+        @MockK
+        private lateinit var stockRepository: StockRepository
 
-    @Test
-    fun `품절 상태에서 사장님이 재고를 추가하면 재고 추가 이벤트를 발행한다`() {
-        // given
-        val productId = "testProductId"
-        val originalStock = 0L
+        @Test
+        fun `품절 상태에서 사장님이 재고를 추가하면 재고 추가 이벤트를 발행한다`() {
+            // given
+            val productId = "testProductId"
+            val originalStock = 0L
 
-        coEvery { stockStateEventPublisher.publishToInStockMsg(any()) } answers {
-            println("published in-stock event")
-        }
+            coEvery { stockStateEventPublisher.publishToInStockMsg(any()) } answers {
+                println("published in-stock event")
+            }
 
-        val increaseAmount = 100L
+            val increaseAmount = 100L
 
-        coEvery { stockRepository.findStockByProductId(productId) } returns originalStock.toString()
-        val increaseProductStockReq = IncreaseProductStockReq(
-            productId,
-            increaseAmount
-        )
-
-        coEvery {
-            stockRepository.increaseStockByProductId(
+            coEvery { stockRepository.findStockByProductId(productId) } returns originalStock.toString()
+            val increaseProductStockReq = IncreaseProductStockReq(
                 productId,
                 increaseAmount
             )
-        } returns originalStock + increaseAmount
 
-        // when
-        runBlocking {
-            stockService.incrProductStock(increaseProductStockReq)
+            coEvery {
+                stockRepository.increaseStockByProductId(
+                    productId,
+                    increaseAmount
+                )
+            } returns originalStock + increaseAmount
+
+            // when
+            runBlocking {
+                stockService.incrProductStock(increaseProductStockReq)
+            }
+
+            // then
+            coVerify {
+                stockStateEventPublisher.publishToInStockMsg(productId)
+            }
+            coVerify {
+                stockRepository.increaseStockByProductId(any(), any())
+            }
         }
 
-        // then
-        coVerify {
-            stockStateEventPublisher.publishToInStockMsg(productId)
+
+        @Test
+        fun `품절 시 카프카 이벤트를 발행한다`() {
+            // given
+            val productId = "testProductId"
+            val originalStock = 2L
+
+            coEvery { stockStateEventPublisher.publishOutOfStockMsg(any()) } answers {
+                println("published out of stock event")
+            }
+
+            val decreaseAmount = 2L
+
+            coEvery {
+                stockRepository.decreaseStockByProductId(productId, decreaseAmount)
+            } returns originalStock - decreaseAmount
+
+            coEvery {
+                stockRepository.increaseStockByProductId(productId, decreaseAmount)
+            } returns originalStock
+
+            // when
+            val decreaseProductStockReq = DecreaseProductStockReq(
+                productId,
+                decreaseAmount
+            )
+            val returnValue = runBlocking {
+                stockService.decProductStock(decreaseProductStockReq)
+            }
+
+            // then
+            assertTrue { returnValue }
+            coVerify {
+                stockStateEventPublisher.publishOutOfStockMsg(any())
+            }
+
+            coVerify { stockRepository.decreaseStockByProductId(productId, decreaseAmount) }
         }
-        coVerify {
-            stockRepository.increaseStockByProductId(any(), any())
+
+        @Test
+        fun `재고 부족으로 인한 주문 취소`() {
+            // given
+            val productId = "testProductId"
+            val originalStock = 2L
+            val decreaseAmount = 3L
+
+            coEvery {
+                stockRepository.decreaseStockByProductId(productId, decreaseAmount)
+            } returns originalStock - decreaseAmount
+
+            coEvery {
+                stockRepository.increaseStockByProductId(productId, decreaseAmount)
+            } returns originalStock
+
+            // when
+            val decreaseProductStockReq = DecreaseProductStockReq(
+                productId,
+                decreaseAmount
+            )
+            val returnValue = runBlocking {
+                stockService.decProductStock(decreaseProductStockReq)
+            }
+
+            // then
+            assertTrue { !returnValue }
+
+            coVerify { stockRepository.decreaseStockByProductId(productId, decreaseAmount) }
+
+            coVerify { stockRepository.increaseStockByProductId(productId, decreaseAmount) }
+
         }
     }
 
+    @SpringBootTest
+    internal class RealStockRepository(
+        @Autowired
+        private val stockService: StockService,
+        @Autowired
+        private val redisTemplate: ReactiveRedisTemplate<String, String>
+    ) {
 
-    @Test
-    fun `품절 시 카프카 이벤트를 발행한다`() {
-        // given
-        val productId = "testProductId"
-        val originalStock = 2L
+        private val productId = "testProductId"
 
-        coEvery { stockStateEventPublisher.publishOutOfStockMsg(any()) } answers {
-            println("published out of stock event")
+        @BeforeEach
+        fun `테스트 상품 재고 저장하기`() {
+            val value = runBlocking { redisTemplate.opsForValue().set(productId, "10").awaitSingle() }
         }
 
-        val decreaseAmount = 2L
+        @Test
+        fun `부하 테스트`() {
+            // given\
+            val stockList = arrayOf(4L, 3L, 2L, 4L, 5L, 2L, 2L)
+            val res = ArrayList<Boolean>()
 
-        coEvery {
-            stockRepository.decreaseStockByProductId(productId, decreaseAmount)
-        } returns originalStock - decreaseAmount
 
-        coEvery {
-            stockRepository.increaseStockByProductId(productId, decreaseAmount)
-        } returns originalStock
+            // when
+            runBlocking {
+                coroutineScope {
+                    (0..6).forEach { i ->
+                        val wasSucceeded =
+                            stockService.decProductStock(DecreaseProductStockReq(productId, stockList[i]))
+                        res.add(wasSucceeded)
+                    }
+                }
+            }
 
-        // when
-        val decreaseProductStockReq = DecreaseProductStockReq(
-            productId,
-            decreaseAmount
-        )
-        val returnValue = runBlocking {
-            stockService.decProductStock(decreaseProductStockReq)
+            // then
+            res.mapIndexed { idx, b ->
+                println("$idx. $b")
+            }
+            val currStock = runBlocking {
+                redisTemplate.opsForValue().get(productId).awaitSingle()
+            }
+            println(currStock)
         }
-
-        // then
-        assertTrue { returnValue }
-        coVerify {
-            stockStateEventPublisher.publishOutOfStockMsg(any())
-        }
-
-        coVerify { stockRepository.decreaseStockByProductId(productId, decreaseAmount) }
-    }
-
-    @Test
-    fun `재고 부족으로 인한 주문 취소`() {
-        // given
-        val productId = "testProductId"
-        val originalStock = 2L
-        val decreaseAmount = 3L
-
-        coEvery {
-            stockRepository.decreaseStockByProductId(productId, decreaseAmount)
-        } returns originalStock - decreaseAmount
-
-        coEvery {
-            stockRepository.increaseStockByProductId(productId, decreaseAmount)
-        } returns originalStock
-
-        // when
-        val decreaseProductStockReq = DecreaseProductStockReq(
-            productId,
-            decreaseAmount
-        )
-        val returnValue = runBlocking {
-            stockService.decProductStock(decreaseProductStockReq)
-        }
-
-        // then
-        assertTrue { !returnValue }
-
-        coVerify { stockRepository.decreaseStockByProductId(productId, decreaseAmount) }
-
-        coVerify { stockRepository.increaseStockByProductId(productId, decreaseAmount) }
-
     }
 }
